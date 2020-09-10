980b1395ce9a1e502431d692fa40b3e7e04e23d9
Joshua Flores <joshuaflores@Joshuas-MBP.lan>
2020-09-10 14:41:35 -0400

    fixed issue with gatsby image in post pages

--- src/templates/post.js
+++ src/templates/post.js
@@ -73,7 +73,7 @@ export const query = graphql`
       heroImage {
         title
         fluid(maxWidth: 1800) {
-          ...GatsbyContentfulFluid_withWebp_noBase64
+          ...GatsbyContentfulFluid
         }
         ogimg: resize(width: 1800) {
           src
