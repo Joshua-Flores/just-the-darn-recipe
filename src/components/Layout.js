ed543ddd03727dbabb3d6342aa11df009c46871f
Joshua Flores <joshuaflores@Joshuas-MBP.lan>
2020-11-26 16:21:06 -0500

    added cookie compliance opt in feature

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,6 +5,9 @@ import theme from '../styles/theme'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import NavBar from '../components/NavBar'
 import { makeStyles } from '@material-ui/core/styles'
+import CookieConsent from 'react-cookie-consent'
+import green from '@material-ui/core/colors/green'
+import red from '@material-ui/core/colors/red'
 
 const useStyles = makeStyles({
   root: {
@@ -33,6 +36,23 @@ const Layout = props => {
     <div className={classes.root}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <CookieConsent
+          location="bottom"
+          buttonText="I ACCEPT"
+          enableDeclineButton
+          declineButtonText="OPT OUT"
+          cookieName="gatsby-gdpr-google-analytics"
+          style={{ background: '#212121' }}
+          declineButtonStyle={{ backgroundColor: red[500] }}
+          buttonStyle={{ backgroundColor: green[500], color: 'white' }}
+        >
+          🍪
+          <strong>
+            &nbsp;ANOTHER DARN COOKIE NOTICE<br></br>
+          </strong>
+          This site uses cookies from Google Analytics to analyze traffic and
+          improve the user experience.
+        </CookieConsent>
         <div className={classes.siteContent}>
           <NavBar />
           <div id="main">{props.children}</div>
